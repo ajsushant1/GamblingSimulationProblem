5e6da5a8be92122f52cbac04b29f7c46be5d665c
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-19 12:40:42 +0530

    ADDED: Condition to check won or lost the bet

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -5,3 +5,15 @@ echo "/******************************************* WELCOME TO GAMBLING SIMULATOR
 #CONSTANTS
 STAKE_PER_DAY=100
 BET_PER_GAME=1
+WIN=1
+
+# TO GET RANDOM VALUE
+randomCheck=$((RANDOM%2))
+
+# CHECKING CONDITION FOR WON OR LOST THE BET
+if [ $randomCheck -eq $WIN ]
+then
+	echo "You won the bet \$$BET_PER_GAME"
+else
+	echo "You loose the bet \$$BET_PER_GAME"
+fi
