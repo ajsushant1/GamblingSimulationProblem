d54acccc77ee2c68f4055430fc79981f5d1b80c2
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-20 11:12:11 +0530

    ADDED: Condtion to compute number of days won and lost and by how much for month

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -6,7 +6,7 @@ echo "/******************************************* WELCOME TO GAMBLING SIMULATOR
 STAKE_PER_DAY=100
 BET_PER_GAME=1
 WIN=1
-TOTAL_DAYS=20
+TOTAL_DAYS=30
 
 # VARIABLES
 stake=$STAKE_PER_DAY
@@ -14,6 +14,10 @@ winLimit=$(($STAKE_PER_DAY+$(($STAKE_PER_DAY*50/100))))
 lossLimit=$(($STAKE_PER_DAY-$(($STAKE_PER_DAY*50/100))))
 totalStake=0
 limit=0
+wonDays=0
+lostDays=0
+totalWinInMonth=0
+totalLostInMonth=0
 
 # FUNCTION TO PLAY GAMBLE FOR A DAY
 function playGamble(){
@@ -37,17 +41,23 @@ for (( day=1; day<=$TOTAL_DAYS; day++ ))
 	gambleResult=$(playGamble)
 	if [ $gambleResult -gt $STAKE_PER_DAY ]
 	then
+		((wonDays++))
 		gambleResult=$(($gambleResult-$stake))
+		totalWinInMonth=$(($totalWinInMonth+$gambleResult))
 	else
+		((lostDays++))
 		gambleResult=$(($gambleResult-$stake))
+		totalLostInMonth=$(($totalLostInMonth+$gambleResult))
 	fi
 	totalStake=$(($totalStake+$gambleResult))
 }
 
-# DISPLAYING TOTAL AMOUNT OF WON OR LOST
+# DISPLAYING TOTAL AMOUNT OF WON OR LOST AFTER PLAYED FOR MONTH
 if [ $totalStake -gt $limit ]
 then
-	echo "You won $totalStake amount after 20 days"
+	echo "You won $totalStake amount after 30 days"
 else
-	echo "You lost $totalStake amount after 20 days"
+	echo "You lost $totalStake amount after 30 days"
 fi
+echo "In month you won $totalWinInMonth in $wonDays days"
+echo "In month you lost $totalLostInMonth in $lostDays days"
